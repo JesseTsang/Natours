b836fc8be993f6cfe5503fdc3914dd07307797c6
Jesse Tsang <1919892+JesseTsang@users.noreply.github.com>
2021-01-26 11:02:18 -0500

    Updates the mongo db schema for more fields

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,52 @@ const tourScheme = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name.'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a durations.'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size.'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price.'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary'],
+    trim: true, // auto remove beginning or the end whitespace
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 // 2. Create a model using the previous created schema
